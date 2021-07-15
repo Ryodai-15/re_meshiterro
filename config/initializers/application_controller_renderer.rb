e7f77157510434233aa64991bc27e8d3fb4089ed
Ryodai-15 <gon.4629.1208@gmail.com>
2021-07-15 00:42:13 +0000

    [gazoutoukou]meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8c76cb33a2912839e471e39aa80d58987d96ab9de9f119e282106b148cd0dd54b831b551eb8bbdd88b8ac313c87d713525dd435db9214aa55221197adc5eaa9a'
